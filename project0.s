ec3c2925e93de0a2821b6fc0f9c9af5d514da2a0
UcheOsujiBison <91709768+UcheOsujiBison@users.noreply.github.com>
2021-09-30 22:33:35 -0400

    Add files via upload

---
+++ project0.s
@@ -0,0 +1,18 @@
+#PROGRAM: Hello, World!
+
+	.data
+out_string:
+	.asciiz "\nHello, World!\n"
+	.text
+
+main:
+	li $v0, 4
+	la $a0, out_string
+	syscall
+
+	lb $t1, 2($a0)
+	addi $t1, $t1, -32
+	sb $t1, 2($a0)
+	syscall
+	li $v0, 10
+	syscall
